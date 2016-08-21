41ec43368e4fc49aba45623a81d9649da3f64906
Niall O'Kane <okanenm@gmail.com>
2016-08-21 12:34:58 -0700

    Create App component

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavLink from './NavLink'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Rod And Thumb</h1>
+      </div>
+    )
+  }
+})
