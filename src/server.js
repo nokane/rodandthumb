ba7096b0a162d372e2dbab56e3cca88052dc159f
Niall O'Kane <okanenm@gmail.com>
2016-08-21 13:41:38 -0700

    Serve css

--- src/server.js
+++ src/server.js
@@ -4,14 +4,24 @@ import compression from 'compression'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { match, RouterContext } from 'react-router'
-import routes from './modules/routes'
+import routes from './routes'
 
 const app = express()
 
+app.use('/', express.static(path.join(__dirname, '../public')))
+
 app.get('*', (req, res) => {
-  match({ routes: routes, location: req.url }, (err, redirect, props) => {
-    const appHtml = renderToString(<RouterContext {...props}/>)
-    res.send(renderPage(appHtml))
+   match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    if (err) {
+      res.status(500).send(err.message)
+    } else if (redirect) {
+      res.redirect(redirect.pathname + redirect.search)
+    } else if (props) {
+      const appHtml = renderToString(<RouterContext {...props}/>)
+      res.send(renderPage(appHtml))
+    } else {
+      res.status(404).send('Not Found')
+    }
   })
 })
 
@@ -20,7 +30,7 @@ function renderPage(appHtml) {
     <!doctype html public="storage">
     <html>
     <meta charset=utf-8/>
-    <title>My First React Router App</title>
+    <title>Rod and Thumb</title>
     <link rel=stylesheet href=/index.css>
     <div id=app>${appHtml}</div>
     <script src="/bundle.js"></script>
