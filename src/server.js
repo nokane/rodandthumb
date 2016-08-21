67ea4bba8ebe7099c22915ef5b7519b8682b4227
Niall O'Kane <okanenm@gmail.com>
2016-08-21 13:04:50 -0700

    Configure Express server

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import path from 'path'
+import compression from 'compression'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { match, RouterContext } from 'react-router'
+import routes from './modules/routes'
+
+const app = express()
+
+app.get('*', (req, res) => {
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    const appHtml = renderToString(<RouterContext {...props}/>)
+    res.send(renderPage(appHtml))
+  })
+})
+
+function renderPage(appHtml) {
+  return `
+    <!doctype html public="storage">
+    <html>
+    <meta charset=utf-8/>
+    <title>My First React Router App</title>
+    <link rel=stylesheet href=/index.css>
+    <div id=app>${appHtml}</div>
+    <script src="/bundle.js"></script>
+   `
+}
+
+var PORT = process.env.PORT || 8080
+app.listen(PORT, function() {
+  console.log('Express server running at localhost:' + PORT)
+})
