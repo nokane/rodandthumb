8d496b3b8626fd66a0f12b138658c50f27d619dc
Niall O'Kane <okanenm@gmail.com>
2016-09-15 00:04:25 -0700

    Refactor App.js component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { Component }  from 'react';
+import Helmet from 'react-helmet';
+import { Link } from 'react-router';
 
-export default React.createClass({
+class App extends Component {
+
   render() {
     return (
       <div>
-        <h1>Rod And Thumb</h1>
+        <Helmet
+          title='Rod and Thumb'
+          titleTemplate='Rod and Thumb - %s'
+          meta={[
+            {'char-set': 'utf-8'},
+            {'name': 'description', 'content': 'DIY - Do it Yourself Fly Fishing and Hitchhiking'}
+          ]}
+        />
+        <nav>
+          <ul>
+            <li><Link to='/'>Users</Link></li>
+          </ul>
+        </nav>
+        {this.props.children}
       </div>
-    )
+    );
   }
-})
+}
+
+export default App;
