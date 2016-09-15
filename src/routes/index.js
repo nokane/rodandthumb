65351237cf8c0e61467c07c96d5cdb5a43a594c3
Niall O'Kane <okanenm@gmail.com>
2016-09-15 00:03:53 -0700

    Move Routes

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Route, IndexRoute } from 'react-router'
-import App from '../components/App'
-
-module.exports = (
-  <Route path="/" component={App}>
-  </Route>
-)
