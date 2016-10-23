eb917a7e871bae9a194f7f9363b46e40f10c8b25
Niall O'Kane <okanenm@gmail.com>
2016-10-22 22:33:34 -0700

    Add sass variables

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -4,8 +4,8 @@ import styles from './Header.scss';
 class Header extends Component {
   render() {
   	return (
-      <div className={styles.fella}>
-        <h1 className={styles.hone}>Rod</h1>
+      <div>
+        <h1>Rod</h1>
         <h2>Hitchhiking and DIY Fly Fishing Abroad</h2>
       </div>
   	);
