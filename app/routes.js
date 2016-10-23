113066bebc1797d1b10a83e87ebb6d2893423e9a
Niall O'Kane <okanenm@gmail.com>
2016-10-22 20:53:22 -0700

    Create basic Home component

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router';
 import App from './components/App';
-import Header from './components/Header';
+import Home from './components/Home';
 
 export default (
-  <Route component={App}>
-   <Route path='/' component={Header} />
+  <Route path='/' component={App}>
+    <Route path='/home' component={Home} />
   </Route>
 );
 
